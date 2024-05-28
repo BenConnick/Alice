cbf285603ca7f917ceae86b28032f6e1c7411bd3
ben <benjaminconnick@gmail.com>
2024-05-27 19:14:55 -0500

    lose text bugfix

--- Assets/Scripts/AppModes/PostFallLoseSlideshowMode.cs
+++ Assets/Scripts/AppModes/PostFallLoseSlideshowMode.cs
@@ -27,6 +27,6 @@ public class PostFallLoseSlideshowMode : SlideshowMode
 
     protected override InGameText GetSlideshowText()
     {
-        return GameplayManager.GetCurrentLevelConfig().LevelText.Data.LevelWinText;
+        return GameplayManager.GetCurrentLevelConfig().LevelText.Data.LevelLoseText;
     }
 }
