6d56e69b2be1bf9b50dd725ee2cea9c239873fdc
ben <benjaminconnick@gmail.com>
2024-08-21 23:09:05 -0500

    change collectable type to keys

--- Assets/Scripts/ScoreViewController.cs
+++ Assets/Scripts/ScoreViewController.cs
@@ -16,7 +16,7 @@ public class ScoreViewController : MonoBehaviour
         FallingGameInstance game = ContextualInputSystem.ActiveGameInstance;
         ScoreObject scores = game.VpScore;
         distanceLabel.PlayTypewriter($"DEPTH:   {scores.MetersMultiplierValue * scores.Meters:00000}", delay: 0.5f);
-        coinsLabel.PlayTypewriter($"RABBITS: {scores.CoinsMultiplierValue * scores.Coins:00000}", delay: 1f);
+        coinsLabel.PlayTypewriter($"KEYS:    {scores.CoinsMultiplierValue * scores.Coins:00000}", delay: 1f);
         heartsLabel.PlayTypewriter($"HEARTS:  {scores.HeartsMultiplierValue * scores.Hearts:00000}", delay: 1.5f);
         totalLabel.PlayTypewriter($"TOTAL: {scores.GetCombinedScore():00000}", delay: 2.5f);
     }
